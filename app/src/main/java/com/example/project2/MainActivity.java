ca86a18f82af3c0fda5d908e2106b3f8279ffdc1
Cesar Gonzalez <CG000000@yahoo.com>
2020-06-05 22:46:54 -0700

    Commit test

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //This is project 3
+        //This is a push
     }
 }
