f73336980d543ba26df95f20cbd9987d44c55b92
Shoraj Manandhar <shorajmanandhar@hotmail.com>
2020-06-06 01:17:25 -0700

    Contributor (Testing: Push)

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //This is project 3
         //This is a push
+
+        // da fook!?
     }
 }
