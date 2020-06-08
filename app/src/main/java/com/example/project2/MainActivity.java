aa4000b8c70eaee5656d54458d40d36ee726a884
Shoraj Manandhar <shorajmanandhar@hotmail.com>
2020-06-07 19:42:25 -0700

    Testing push to github  - contains just the basic 2 fragments
     Tryna create a new fragment which will contain gallery view

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.project2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is project 3
-        //This is a push
 
-        // da fook!?
+
     }
 }
